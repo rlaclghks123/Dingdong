de76873b499f9a0ab28a404c76b1d1189da96b50
chiman <rlaclghks123@naver.com>
2023-08-03 16:18:17 +0900

    Feat: 공통적인 컴포넌트 추가 :  MainHeader, SortTag, SortModal

--- src/pages/StoreCategoryPage/StoreCategoryPage.tsx
+++ src/pages/StoreCategoryPage/StoreCategoryPage.tsx
@@ -1,15 +1,41 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { css } from '@emotion/react';
+
 import Layout from '../../layouts/Layout';
-import { headerLeftData, headerRightData } from '../DeliveryPage/DeliveryPage';
+import { headerLeftData, headerRightData, sortList } from '../DeliveryPage/DeliveryPage';
 import MainHeader from '../../components/header/MainHeader/MainHeader';
 import Nav from '../../components/nav/Nav';
+import SortTag from '../../components/main/SortTag/SortTag';
+import SortModal from '../../components/Modal/SortModal';
+
+export const Wrapper = (isClicked: boolean) => css`
+  background-color: white;
+  ${isClicked &&
+  `
+      position: fixed;
+      left: 0px;
+      right: 0px;
+      transform: translate(50% 0%);
+ `}
+`;
 
 const StoreCategory = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
-    <Layout>
-      <MainHeader leftItem={headerLeftData} rightItem={headerRightData} />
-      <div>1</div>
-      <Nav />
-    </Layout>
+    <>
+      <div css={Wrapper(isClicked)}>
+        <Layout>
+          <MainHeader leftItem={headerLeftData} rightItem={headerRightData} />
+          <div>
+            <SortTag setIsClicked={setIsClicked} />
+          </div>
+          <Nav />
+        </Layout>
+        {isClicked && <SortModal sortList={sortList} setIsClicked={setIsClicked} />}
+      </div>
+    </>
   );
 };
 
