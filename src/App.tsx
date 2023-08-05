e92ba5c763bd5158d03c394f9be212354b356f06
chiman <rlaclghks123@naver.com>
2023-08-05 15:49:16 +0900

    Feat: ScrollToTop 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import DeleveryPage from './pages/DeliveryPage/DeliveryPage';
 import StoreCategoryPage from './pages/StoreCategoryPage/StoreCategoryPage';
+import ScrollToTop from './components/common/ScrollToTop/ScrollToTop.tsx';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path={'/'} element={<HomePage />} />
         <Route path={'/delivery'} element={<DeleveryPage />} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { ThemeProvider } from '@emotion/react';
 
 import theme from './styles/theme.ts';
 import GlobalStyle from './styles/GlobalStyle.tsx';
-import { ThemeProvider } from '@emotion/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
