d133ef6366b195ae7d48e065b4eb81b1de852c06
chiman <rlaclghks123@naver.com>
2023-08-30 13:50:31 +0900

    Rename: StoreListItem => ShopListItem 으로 컴포넌트명 변경

--- src/components/common/ShopListsItem/ShopListsItem.tsx
+++ src/components/common/ShopListsItem/ShopListsItem.tsx
@@ -9,11 +9,11 @@ import {
   DescriptionTitle,
   StarStyle,
   DescriptionDeliveryBox,
-} from './StoreListsItem.style';
-import { Item } from '../../../pages/HomePage/HomePage';
+} from './ShopListsItem.style';
+import { CreateShopListDataResponse } from '../../../mocks/data/dingdongWorld';
 
-interface StoreListsItemdProps {
-  items: Item[];
+interface Props {
+  items: CreateShopListDataResponse[];
   itemWidth: number;
   size: string;
   imgWidth?: number | string;
@@ -21,36 +21,36 @@ interface StoreListsItemdProps {
   startPosition?: number;
 }
 
-const StoreListsItem = ({ items, itemWidth, size, carouselItemsRef, startPosition }: StoreListsItemdProps) => {
+const ShopListsItem = ({ items, itemWidth, size, carouselItemsRef, startPosition }: Props) => {
   const navigate = useNavigate();
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     const itemId = Number(e.currentTarget.dataset.id);
     if (carouselItemsRef && startPosition !== e.clientX) return;
-    navigate(items[itemId].link || '');
+    navigate(items[itemId].uid || '');
   };
 
   return (
     <>
-      {items.map((item, index) => (
-        <button css={ItemBox(itemWidth, size)} key={index} onClick={handleClick} data-id={index}>
-          <img css={Img(size)} src={item.img} alt={`${item.title} 사진`} />
+      {items?.map((item, index) => (
+        <button css={ItemBox(itemWidth, size)} key={item.uid} onClick={handleClick} data-id={index}>
+          <img css={Img(size)} src={item.info.image} alt={`${item.info.name} 사진`} />
 
           <div css={DescriptionBox(size)}>
             <div css={DescriptionTitleBox(size)}>
-              <span css={DescriptionTitle}>{item.title}</span>
+              <span css={DescriptionTitle}>{item.info.title}</span>
               <span>
                 <svg css={StarStyle} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
                   <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
                 </svg>
-                {`${item.star}점`}
+                {`${item.info.rate}점`}
               </span>
             </div>
 
             <div>
               <div css={DescriptionDeliveryBox(size)}>
-                <span>배달 : {item.deliveryTime}</span>
-                <span>배달팁 : {`${item?.deliveryTip?.toLocaleString()}원`}</span>
+                <span>배달 : {item.info.deliveryTime}</span>
+                <span>배달팁 : {`${item.info.deliveryFee?.toLocaleString()}원`}</span>
               </div>
             </div>
           </div>
@@ -60,4 +60,4 @@ const StoreListsItem = ({ items, itemWidth, size, carouselItemsRef, startPositio
   );
 };
 
-export default StoreListsItem;
+export default ShopListsItem;
