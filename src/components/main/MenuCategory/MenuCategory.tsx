853571fd0398a0e0201ac053b3715cf6348be25a
chiman <rlaclghks123@naver.com>
2023-09-01 00:54:37 +0900

    refactor: MenuCategory Mock Data MSW Data로 수정

--- src/components/main/MenuCategory/MenuCategory.tsx
+++ src/components/main/MenuCategory/MenuCategory.tsx
@@ -1,53 +1,27 @@
-import { ReactNode, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { css } from '@emotion/react';
-import { menuItems } from '../../../pages/DeliveryPage/DeliveryPage';
-import {
-  Wrapper,
-  MenuItemBox,
-  LinkBox,
-  Name,
-  EmptyItems,
-  Dot,
-  GRID_LENGTH,
-  MenuCategoryBox,
-} from './MenuCategory.style';
 
-interface MenuItemsProps {
-  link: string;
-  name: string;
-  icon: ReactNode;
-}
+import { Wrapper, MenuItemBox, LinkBox, Name, MenuCategoryBox } from './MenuCategory.style';
+import useGetFoodCategories from '../../../hooks/useGetFoodCategories';
+import { CreateFoodCategoriesDataResponse } from '../../../mocks/data/dingdongWorld';
 
 const MenuCategory = () => {
-  const [itemLists, setItemLists] = useState(menuItems);
-
-  const getNumberOfDots = (itemLists: MenuItemsProps[]) => {
-    const emptyItemsCount = GRID_LENGTH - (itemLists.length % GRID_LENGTH);
-    const emptyItemList = Array.from({ length: emptyItemsCount }).fill(null);
-    if (emptyItemList.length !== GRID_LENGTH) return emptyItemList;
-  };
-
-  const emptyItemList = getNumberOfDots(itemLists) || [];
+  const { data, isLoading, isError } = useGetFoodCategories();
 
   return (
     <section css={Wrapper}>
-      <div css={MenuCategoryBox}>
-        {itemLists.map((item, idx) => (
-          <div css={MenuItemBox} key={idx}>
-            <Link css={LinkBox} to={item.link}>
-              <span>{item.icon}</span>
-              <span css={Name}>{item.name}</span>
-            </Link>
-          </div>
-        ))}
-
-        {emptyItemList.map((_, idx) => (
-          <div css={EmptyItems} key={`empty-${idx}`}>
-            <span css={Dot} />
-          </div>
-        ))}
-      </div>
+      {isLoading && <div>로딩중..</div>}
+      {!isLoading && (
+        <div css={MenuCategoryBox}>
+          {data?.map((item: CreateFoodCategoriesDataResponse) => (
+            <div css={MenuItemBox} key={item.uid}>
+              <Link css={LinkBox} to={`/delivery/category/${item.title}`}>
+                <span dangerouslySetInnerHTML={{ __html: item.SVG }} />
+                <span css={Name}>{item.title}</span>
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
     </section>
   );
 };
