956a2aebf30a2376a2d98a4a693e9aa3ff7cc342
chiman <rlaclghks123@naver.com>
2023-09-01 01:06:24 +0900

    Refactor: SearchBar 컴포넌트에서 Dropdown 분리

--- src/components/main/SearchBar/SearchBar.tsx
+++ src/components/main/SearchBar/SearchBar.tsx
@@ -1,101 +1,50 @@
-import { useEffect, useState } from 'react';
-import { DropDownBox, DropDownItem, Input, Wrapper } from './SearchBar.style';
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
-const PREVIEW_LIST = ['bhc', 'bbq', '굽네치킨', '지코바', '맥도날드', '버거킹', '도미노'];
+import { Input, Wrapper } from './SearchBar.style';
+import useDropdown from '../../../hooks/useDropdown';
+import Dropdown from '../../common/Dropdown/Dropdown';
 
 const SearchBar = () => {
   const [inputValue, setInputValue] = useState('');
-  const [isFocus, setIsFocus] = useState(false);
-  const [dropDownList, setDropDownList] = useState(PREVIEW_LIST);
-  const [dropDownItemIndex, setDropDownItemIndex] = useState(-1);
-  const navigate = useNavigate();
 
-  const showDropDownList = () => {
-    if (inputValue === '') {
-      setDropDownList([]);
-    } else {
-      const copyPreviewList = [...PREVIEW_LIST].filter(
-        textItem => textItem.includes(inputValue.toLocaleLowerCase()) || textItem.includes(inputValue.toUpperCase())
-      );
-      setDropDownList(copyPreviewList);
-    }
-  };
+  const {
+    handleDropDownKeyDown,
+    isFocus,
+    handleClickDropDownList,
+    dropDownList,
+    setDropDownItemIndex,
+    dropDownItemIndex,
+  } = useDropdown(inputValue);
 
   const changeInputValue = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
 
-  const handleDropDownKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'ArrowDown') {
-      dropDownItemIndex === dropDownList.length - 1 ? setDropDownItemIndex(0) : setDropDownItemIndex(prev => prev + 1);
-    }
-
-    if (event.key === 'ArrowUp') {
-      dropDownItemIndex === 0 ? setDropDownItemIndex(dropDownList.length - 1) : setDropDownItemIndex(prev => prev - 1);
-    }
-
-    if (event.key === 'Enter') {
-      let keyword = dropDownList[dropDownItemIndex] ? dropDownList[dropDownItemIndex] : inputValue;
-      navigate(`/search?keyword=${keyword}`);
-    }
-  };
-
-  const handleClickSearchBox = (e: MouseEvent) => {
-    const isFocus = (e.target as HTMLElement).closest('form')?.dataset.id;
-    isFocus ? setIsFocus(true) : setIsFocus(false);
-  };
-
-  const handleClickDropDownList = (dropDownItem: string) => {
-    navigate(`/search?keyword=${dropDownItem}`);
-  };
-
-  useEffect(() => {
-    showDropDownList();
-  }, [inputValue]);
-
-  useEffect(() => {
-    window.addEventListener('click', handleClickSearchBox);
-    return () => {
-      window.removeEventListener('click', handleClickSearchBox);
-    };
-  }, []);
-
-  const DropDown = () => {
-    return (
-      <div css={DropDownBox}>
-        {dropDownList.length === 0 && <div css={DropDownItem}>해당하는 단어가 없습니다</div>}
-        {dropDownList.map((dropDownItem, dropDownIndex) => {
-          return (
-            <div
-              css={DropDownItem}
-              key={`${dropDownItem}-${dropDownIndex}`}
-              onClick={() => handleClickDropDownList(dropDownItem)}
-              onMouseOver={() => setDropDownItemIndex(dropDownIndex)}
-              className={dropDownItemIndex === dropDownIndex ? 'selected' : ''}
-            >
-              {dropDownItem}
-            </div>
-          );
-        })}
-      </div>
-    );
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (inputValue.trim() === '') return;
   };
 
   return (
-    <form css={Wrapper} data-id={'isInputFocus'} onSubmit={(e: React.FormEvent<HTMLFormElement>) => e.preventDefault()}>
-      <div>
-        <input
-          css={Input}
-          autoComplete="off"
-          type="text"
-          name="keyword"
-          value={inputValue}
-          onChange={changeInputValue}
-          onKeyUp={handleDropDownKeyUp}
+    <form css={Wrapper} data-id={'isInputFocus'} onSubmit={handleSubmit}>
+      <input
+        css={Input}
+        autoComplete="off"
+        type="text"
+        name="keyword"
+        value={inputValue}
+        onChange={changeInputValue}
+        onKeyDown={handleDropDownKeyDown}
+      />
+
+      {isFocus && (
+        <Dropdown
+          handleClickDropDownList={handleClickDropDownList}
+          dropDownList={dropDownList}
+          setDropDownItemIndex={setDropDownItemIndex}
+          dropDownItemIndex={dropDownItemIndex}
         />
-        {isFocus && <DropDown />}
-      </div>
+      )}
     </form>
   );
 };
