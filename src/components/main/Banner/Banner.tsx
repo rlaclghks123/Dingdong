3d008838ab6ba30cca47e46b036387758b016d1d
chiman <rlaclghks123@naver.com>
2023-09-01 00:47:09 +0900

    refactor: Banner 컴포넌트 MSW Mock Data로 수정

--- src/components/main/Banner/Banner.tsx
+++ src/components/main/Banner/Banner.tsx
@@ -1,45 +1,57 @@
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
 
 import useDrag from '../../../hooks/useDrag';
+import useGetShopList from '../../../hooks/useGetShopList';
 import DragCarousel from '../../common/DragCarousel/DragCarousel';
 import { itemWidthObj } from '../../../constants/itemConstatns';
-import { itemList } from '../../../pages/DeliveryPage/DeliveryPage';
+
 import { DragItemWrapper, Img, TitleBox } from './Banner.style';
+import { CreateShopListDataResponse } from '../../../mocks/data/dingdongWorld';
 
 const Banner = () => {
   const navigate = useNavigate();
-  const [itemLists, setItemLists] = useState(itemList);
+  const { data, isLoading, isError } = useGetShopList();
 
   const { carouselItemsRef, isDragging, isMobile, startPosition, endPosition } = useDrag({
-    items: itemLists,
+    items: data,
     itemWidth: itemWidthObj.banner,
+    isLoading,
     autoPlay: true,
     autoPlayDuration: 3000,
   });
 
-  const slideIndicator = `${Math.floor(-endPosition / itemWidthObj.banner) + 1} / ${itemList.length}`;
+  const slideIndicator = data ? `${Math.floor(-endPosition / itemWidthObj.banner) + 1} / ${data.length}` : '0';
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    if (startPosition === e.clientX) navigate(itemLists[Number(e.currentTarget.dataset.id)].link || '');
+    if (startPosition === e.clientX)
+      navigate(
+        `/delivery/category/${data[Number(e.currentTarget.dataset.id)].info.title}/${
+          data[Number(e.currentTarget.dataset.id)].uid || ''
+        }`
+      );
   };
 
   return (
-    <DragCarousel
-      isMobile={isMobile}
-      carouselItemsRef={carouselItemsRef}
-      isDragging={isDragging}
-      slideIndicator={slideIndicator}
-    >
-      <div css={DragItemWrapper}>
-        {itemLists.map((item, index) => (
-          <button key={index} onClick={handleClick} data-id={index}>
-            <img css={Img(itemWidthObj.banner)} src={item.img} alt={`${item.title} 사진`} />
-            <span css={TitleBox}>{item.title}</span>
-          </button>
-        ))}
-      </div>
-    </DragCarousel>
+    <>
+      {isLoading && <div>로딩중..</div>}
+      {!isLoading && (
+        <DragCarousel
+          isMobile={isMobile}
+          carouselItemsRef={carouselItemsRef}
+          isDragging={isDragging}
+          slideIndicator={slideIndicator}
+        >
+          <div css={DragItemWrapper}>
+            {data?.map((item: CreateShopListDataResponse, index: number) => (
+              <button key={item.uid} onClick={handleClick} data-id={index}>
+                <img css={Img(itemWidthObj.banner)} src={item.info.image} alt={`${item.info.title} 사진`} />
+                <span css={TitleBox}>{item.info.title}</span>
+              </button>
+            ))}
+          </div>
+        </DragCarousel>
+      )}
+    </>
   );
 };
 
