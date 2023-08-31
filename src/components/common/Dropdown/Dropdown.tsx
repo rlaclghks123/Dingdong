b76160a96963ad9f748a87233cce1049561fd034
chiman <rlaclghks123@naver.com>
2023-08-31 17:03:45 +0900

    feat: Dropdown 컴포넌트 및 Search Bar 생성

---
+++ src/components/common/Dropdown/Dropdown.tsx
@@ -0,0 +1,48 @@
+import { css } from '@emotion/react';
+import theme from '../../../styles/theme';
+
+const DropDownBox = css`
+  margin: -10px auto;
+  padding-top: 10px;
+  background-color: white;
+  height: 100px;
+`;
+
+const DropDownItem = css`
+  padding: 0 16px;
+
+  &.selected {
+    cursor: pointer;
+    background-color: ${theme.grey200};
+  }
+`;
+
+interface Props {
+  handleClickDropDownList: (dropDownItem: string) => void;
+  dropDownList: string[];
+  setDropDownItemIndex: React.Dispatch<React.SetStateAction<number>>;
+  dropDownItemIndex: number;
+}
+
+const Dropdown = ({ handleClickDropDownList, dropDownList, setDropDownItemIndex, dropDownItemIndex }: Props) => {
+  return (
+    <div css={DropDownBox}>
+      {dropDownList.length === 0 && <div css={DropDownItem}>해당하는 단어가 없습니다</div>}
+      {dropDownList.map((dropDownItem, dropDownIndex) => {
+        return (
+          <div
+            css={DropDownItem}
+            key={`${dropDownItem}-${dropDownIndex}`}
+            onClick={() => handleClickDropDownList(dropDownItem)}
+            onMouseOver={() => setDropDownItemIndex(dropDownIndex)}
+            className={dropDownItemIndex === dropDownIndex ? 'selected' : ''}
+          >
+            <a>{dropDownItem}</a>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/components/main/SearchBar/SearchBar.style.ts
+++ src/components/main/SearchBar/SearchBar.style.ts
@@ -1,29 +1,11 @@
 import { css } from '@emotion/react';
-import commonStyle from '../../../styles/common';
-import theme from '../../../styles/theme';
 
 export const Wrapper = css`
-  margin: ${commonStyle.boxSideMargin};
+  margin: 24px 16px 16px 16px;
 `;
 
 export const Input = css`
   width: 100%;
   padding: 16px;
   border-radius: 16px;
-`;
-
-export const DropDownBox = css`
-  margin: -10px auto;
-  padding-top: 10px;
-  background-color: white;
-  height: 100px;
-`;
-
-export const DropDownItem = css`
-  padding: 0 16px;
-
-  &.selected {
-    cursor: pointer;
-    background-color: ${theme.grey200};
-  }
 `;
