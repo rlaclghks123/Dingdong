8baaa38da5662723eec0f5c7cc369981594d63ea
chiman <rlaclghks123@naver.com>
2023-09-01 15:10:07 +0900

    Feat: WholeMenuList 컴포넌트 생성

---
+++ src/components/main/WholeMenuList/WholeMenuList.tsx
@@ -0,0 +1,41 @@
+import { css } from '@emotion/react';
+import MenuListItem from '../../common/MenuListItem/MenuListItem';
+import useGetShopList from '../../../hooks/useGetShopList';
+
+const Section = css`
+  margin-top: 16px;
+  width: 360px;
+  min-width: 360px;
+`;
+
+const Header = css`
+  font-weight: 800;
+  font-size: 24px;
+  margin-top: 1px solid rgba(0, 0, 0, 1);
+`;
+
+const MenuListBox = css`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 8px;
+`;
+
+const MunuListSize = {
+  small: 'SMALL',
+  medium: 'MEDIUM',
+  large: 'LARGE',
+};
+
+const WholeMenuList = () => {
+  const { data, isLoading, isError } = useGetShopList();
+
+  return (
+    <section css={Section}>
+      <div css={Header}>전체 상품</div>
+      <div css={MenuListBox}>
+        <MenuListItem items={data} itemWidth={172} size={MunuListSize.large} />
+      </div>
+    </section>
+  );
+};
+export default WholeMenuList;
